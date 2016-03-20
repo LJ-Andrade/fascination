16abb8ed131c4e064bab0c9a2f0bd956921508e3
javzero <javzero@hotmail.com>
2016-03-20 03:35:00 -0300

    First Upload

---
+++ files/includes/inc.web.head.php
@@ -0,0 +1,27 @@
+<title>Fascination</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Eshop Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<!-- Stylesheets -->
+<link href="../../../skin/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="../../../skin/css/flexslider.css" rel="stylesheet" type="text/css" media="all" />
+<link href="../../../skin/css/webstyles.css" rel="stylesheet" type="text/css" media="all" />
+
+
+<!-- Smart Menu -->
+<link href="../../../skin/css/smartmenus.bootstrap.css" rel="stylesheet" type="text/css" />
+<link href="../../../skin/css/smartmenus.theme.css" rel="stylesheet" type="text/css" />
+<!--  Optional -->
+
+
+<script type="application/x-javascript"> 
+	addEventListener("load", function() { 
+		setTimeout(hideURLbar, 0); }, false); 
+			function hideURLbar(){ 
+			window.scrollTo(0,1); } 
+</script>
+
+
+
+
